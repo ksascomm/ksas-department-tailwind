25ef03fb5828bc9cb01b58d69956ab8f184dc9c3
ksascomm <ksasweb@jhu.edu>
2023-09-05 09:30:25 -0400

    Add ARIA landmark

--- tribe-events/single-event.php
+++ tribe-events/single-event.php
@@ -72,75 +72,76 @@ $cost  = tribe_get_formatted_cost( $event_id );
 
 ?>
 
-<div id="tribe-events-content" class="tribe-events-single">
-<h2>hello</h2>
-	<p class="tribe-events-back">
-		<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'ksas-department-tailwind' ), $events_label_plural ); ?></a>
-	</p>
+<main id="site-content" class="site-main prose sm:prose lg:prose-lg">
+	<div id="tribe-events-content" class="tribe-events-single">
+		<p class="tribe-events-back">
+			<a href="<?php echo esc_url( tribe_get_events_link() ); ?>"> <?php printf( '&laquo; ' . esc_html_x( 'All %s', '%s Events plural label', 'ksas-department-tailwind' ), $events_label_plural ); ?></a>
+		</p>
 
-	<!-- Notices -->
-	<?php tribe_the_notices(); ?>
+		<!-- Notices -->
+		<?php tribe_the_notices(); ?>
 
-	<?php echo $title; ?>
+		<?php echo $title; ?>
 
-	<div class="tribe-events-schedule tribe-clearfix">
-		<?php echo tribe_events_event_schedule_details( $event_id, '<h2>', '</h2>' ); ?>
-		<?php if ( ! empty( $cost ) ) : ?>
-			<span class="tribe-events-cost"><?php echo esc_html( $cost ); ?></span>
-		<?php endif; ?>
-	</div>
+		<div class="tribe-events-schedule tribe-clearfix">
+			<?php echo tribe_events_event_schedule_details( $event_id, '<h2>', '</h2>' ); ?>
+			<?php if ( ! empty( $cost ) ) : ?>
+				<span class="tribe-events-cost"><?php echo esc_html( $cost ); ?></span>
+			<?php endif; ?>
+		</div>
 
-	<!-- Event header -->
-	<div id="tribe-events-header" <?php tribe_events_the_header_attributes(); ?>>
-		<!-- Navigation -->
-		<nav class="tribe-events-nav-pagination" aria-label="<?php printf( esc_html__( '%s Navigation', 'ksas-department-tailwind' ), $events_label_singular ); ?>">
-			<ul class="tribe-events-sub-nav">
-				<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ); ?></li>
-				<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ); ?></li>
-			</ul>
-			<!-- .tribe-events-sub-nav -->
-		</nav>
-	</div>
-	<!-- #tribe-events-header -->
+		<!-- Event header -->
+		<div id="tribe-events-header" <?php tribe_events_the_header_attributes(); ?>>
+			<!-- Navigation -->
+			<nav class="tribe-events-nav-pagination" aria-label="<?php printf( esc_html__( '%s Navigation', 'ksas-department-tailwind' ), $events_label_singular ); ?>">
+				<ul class="tribe-events-sub-nav">
+					<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ); ?></li>
+					<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ); ?></li>
+				</ul>
+				<!-- .tribe-events-sub-nav -->
+			</nav>
+		</div>
+		<!-- #tribe-events-header -->
 
-	<?php
-	while ( have_posts() ) :
-		the_post();
-		?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<!-- Event featured image, but exclude link -->
-			<?php echo tribe_event_featured_image( $event_id, 'full', false ); ?>
-
-			<!-- Event content -->
-			<?php do_action( 'tribe_events_single_event_before_the_content' ); ?>
-			<div class="tribe-events-single-event-description tribe-events-content">
-				<?php the_content(); ?>
-			</div>
-			<!-- .tribe-events-single-event-description -->
-			<?php do_action( 'tribe_events_single_event_after_the_content' ); ?>
-
-			<!-- Event meta -->
-			<?php do_action( 'tribe_events_single_event_before_the_meta' ); ?>
-			<?php tribe_get_template_part( 'modules/meta' ); ?>
-			<?php do_action( 'tribe_events_single_event_after_the_meta' ); ?>
-		</div> <!-- #post-x -->
 		<?php
-		if ( get_post_type() == Tribe__Events__Main::POSTTYPE && tribe_get_option( 'showComments', false ) ) {
-			comments_template();}
-		?>
-	<?php endwhile; ?>
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<!-- Event featured image, but exclude link -->
+				<?php echo tribe_event_featured_image( $event_id, 'full', false ); ?>
 
-	<!-- Event footer -->
-	<div id="tribe-events-footer">
-		<!-- Navigation -->
-		<nav class="tribe-events-nav-pagination" aria-label="<?php printf( esc_html__( '%s Navigation', 'ksas-department-tailwind' ), $events_label_singular ); ?>">
-			<ul class="tribe-events-sub-nav">
-				<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ); ?></li>
-				<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ); ?></li>
-			</ul>
-			<!-- .tribe-events-sub-nav -->
-		</nav>
-	</div>
-	<!-- #tribe-events-footer -->
+				<!-- Event content -->
+				<?php do_action( 'tribe_events_single_event_before_the_content' ); ?>
+				<div class="tribe-events-single-event-description tribe-events-content">
+					<?php the_content(); ?>
+				</div>
+				<!-- .tribe-events-single-event-description -->
+				<?php do_action( 'tribe_events_single_event_after_the_content' ); ?>
 
-</div><!-- #tribe-events-content -->
+				<!-- Event meta -->
+				<?php do_action( 'tribe_events_single_event_before_the_meta' ); ?>
+				<?php tribe_get_template_part( 'modules/meta' ); ?>
+				<?php do_action( 'tribe_events_single_event_after_the_meta' ); ?>
+			</div> <!-- #post-x -->
+			<?php
+			if ( get_post_type() == Tribe__Events__Main::POSTTYPE && tribe_get_option( 'showComments', false ) ) {
+				comments_template();}
+			?>
+		<?php endwhile; ?>
+
+		<!-- Event footer -->
+		<div id="tribe-events-footer">
+			<!-- Navigation -->
+			<nav class="tribe-events-nav-pagination" aria-label="<?php printf( esc_html__( '%s Navigation', 'ksas-department-tailwind' ), $events_label_singular ); ?>">
+				<ul class="tribe-events-sub-nav">
+					<li class="tribe-events-nav-previous"><?php tribe_the_prev_event_link( '<span>&laquo;</span> %title%' ); ?></li>
+					<li class="tribe-events-nav-next"><?php tribe_the_next_event_link( '%title% <span>&raquo;</span>' ); ?></li>
+				</ul>
+				<!-- .tribe-events-sub-nav -->
+			</nav>
+		</div>
+		<!-- #tribe-events-footer -->
+
+	</div><!-- #tribe-events-content -->
+</main>
