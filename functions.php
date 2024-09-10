b805689ba34d5f4f1fbb65353866e38666bd9677
ksascomm <ksasweb@jhu.edu>
2024-09-10 10:17:02 -0400

    fix bold link style

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'KSAS_DEPARTMENT_TAILWIND_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'KSAS_DEPARTMENT_TAILWIND_VERSION', '4.2.1' );
+	define( 'KSAS_DEPARTMENT_TAILWIND_VERSION', '4.2.2' );
 }
 
 if ( ! function_exists( 'ksas_department_tailwind_setup' ) ) :
