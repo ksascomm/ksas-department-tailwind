a50406a5278c682177fb18f0633fd2753374aa35
ksascomm <ksasweb@jhu.edu>
2024-10-03 13:19:41 -0400

    4.2.1: Track directory quicksearch

--- resources/js/isotope.js
+++ resources/js/isotope.js
@@ -1,7 +1,7 @@
 /**
  * File isotope.js.
  *
- * Customized isotope script for Research Projects Page Template
+ * Customized isotope script for People Directory Page Template
  */
 
 jQuery(document).ready(function ($) {
@@ -116,4 +116,49 @@ jQuery(document).ready(function ($) {
       }, 1000);
     });
   })(jQuery);
+  (function() {
+    // Set searchField to the search input field.
+    // Set timeout to the time you want to wait after the last character in milliseconds.
+    // Set minLength to the minimum number of characters that constitutes a valid search.
+    var searchField = document.querySelector('#id_search'),
+      timeout = 1500,
+      minLength = 3;
+  
+    var textEntered = false;
+  
+    var timer, searchText;
+    
+    var handleInput = function() {
+      searchText = searchField ? searchField.value : '';
+      if (searchText.length < minLength) {
+      return;
+      }
+      window.dataLayer.push({
+      'event': 'peopledirectoryInput',
+      'searchTerm': searchText
+      });
+      textEntered = false;
+    };
+    
+    var startTimer = function(e) {
+      textEntered = true;
+      window.clearTimeout(timer);
+      if (e.keyCode === 13) {
+      handleInput();
+      return;
+      }
+      timer = setTimeout(handleInput, timeout);
+    };
+    
+    if (searchField !== null) {
+      searchField.addEventListener('keydown', startTimer, true);
+      searchField.addEventListener('blur', function() {
+      if (textEntered) {
+        window.clearTimeout(timer);
+        handleInput();
+      }
+      }, true);
+    }
+    })();
 });
+
